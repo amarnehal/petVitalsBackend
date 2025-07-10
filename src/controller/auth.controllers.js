60a28c76e1d41169cc333d9772084ebcd1c2848c
Amarveer <amar.nehal@gmail.com>
2025-07-10 06:56:15 +0530

    Fix Cookies to support cross-site in production

--- src/controller/auth.controllers.js
+++ src/controller/auth.controllers.js
@@ -76,7 +76,12 @@ const logIn = asyncHandler(async (req, res) => {
   }
 
   const accessToken = existingUser.generateAccessToken();
-  res.cookie("token", accessToken, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 });
+ res.cookie("token", accessToken, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',    // true on Render
+  sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',  // None for cross-origin
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
   const safeUser = {
     id: existingUser._id,
