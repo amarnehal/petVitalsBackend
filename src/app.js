92cfdde9fb377a0b57f6c89b15ff4c625f277051
Amarveer <amar.nehal@gmail.com>
2025-07-14 02:39:50 +0530

    fix: added cors fronteend url

--- src/app.js
+++ src/app.js
@@ -13,14 +13,23 @@ dotenv.config(
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://pet-vitals-frontend.vercel.app"
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended:true}))
 
 /////////////////////// Routes //////////////
 
